81d52e3ecd415cb2397ded55c3f38360cd6216c9
Cassiopeia-thech <57096209+Cassiopeia-thech@users.noreply.github.com>
2020-08-19 18:41:06 +0300

    merge and deployed

--- config/keys.dev.js
+++ config/keys.dev.js
@@ -1,7 +1,4 @@
 module.exports = {
-    mongoURI: 
-    "mongodb+srv://fasile:3r7ELZer8CJrSJmS@cluster0.ws4v3.mongodb.net/shega?retryWrites=true&w=majority",
-    // mongoURI: "mongodb://localhost:27017/shega",
+    mongoURI: "mongodb://localhost:27017/shega",
     secretOrKey: "FxUum76z"
 };
-//3r7ELZer8CJrSJmS
