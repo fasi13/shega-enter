9cf8d33e957855b401bc1bd451bcf969548aa26b
Cassiopeia-thech <57096209+Cassiopeia-thech@users.noreply.github.com>
2020-08-19 17:28:16 +0300

    Prepare to deploy

---
+++ config/keys.dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 
+    "mongodb+srv://fasile:3r7ELZer8CJrSJmS@cluster0.ws4v3.mongodb.net/shega?retryWrites=true&w=majority",
+    // mongoURI: "mongodb://localhost:27017/shega",
+    secretOrKey: "FxUum76z"
+};
+//3r7ELZer8CJrSJmS
--- server.js
+++ server.js
@@ -22,18 +22,20 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(bodyParser.json());
 
-app.listen(9000);
-
 app.use(passport.initialize());
 
 // define Routes
 app.use('/api', admins);
 app.use('/api/jokes', jokes);
-app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
