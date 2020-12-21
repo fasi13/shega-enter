8a4e2a64c469844e05cea3f3308ac3bb161e895f
FasileEndalk <sonofendalkachew@gmail.com>
2020-12-21 13:57:10 +0300

    adding admin link

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -5,110 +5,110 @@ import { connect } from "react-redux";
 import { loginAdmin } from "../../actions/authActions";
 import classnames from "classnames";
 
-import './Login.css';
+import "./Login.css";
 
 class Login extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: "",
-            password: "",
-            errors: {}
-        };
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
     }
 
-    componentDidMount() {
-        if (this.props.auth.isAuthenticated) {
-            this.props.history.push("/dashboard");
-        }
-    };
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.auth.isAuthenticated) {
-            this.props.history.push("/dashboard");
-        }
-
-        if (nextProps.errors) {
-            this.setState({
-                errors: nextProps.errors
-            });
-        }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors,
+      });
     }
+  }
 
-    onChange = e => {
-        this.setState({ [e.target.id]: e.target.value });
+  onChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const adminData = {
+      email: this.state.email,
+      password: this.state.password,
     };
+    this.props.loginAdmin(adminData);
+  };
 
-    onSubmit = e => {
-        e.preventDefault();
-        const adminData = {
-            email: this.state.email,
-            password: this.state.password
-        };
-        this.props.loginAdmin(adminData);
-    };
-
-    render() {
-        const { errors } = this.state;
-        return (
-            <div className="container">
-                <div className="row mt-5">
-                    <div className="col-md-4 mx-auto mt-5 card shadow-lg">
-                        <div className="card-body p-1">
-                            <h2 className="text-center text-primary mt-3">Login</h2>
-                            <a className="backToHome" href="/home" tag={Link}><h2>Home</h2></a>
-                            <form noValidate onSubmit={this.onSubmit} className="white">
-                                <label htmlFor="email">Email</label>
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.email}
-                                    error={errors.email}
-                                    id="email"
-                                    type="email"
-                                    className={classnames("form-control", {
-                                        invalid: errors.email
-                                    })}
-                                />
-                                <span className="text-danger">{errors.email}</span>
-                                <br/>
-                                <label htmlFor="password">Password</label>
-                                <input
-                                    onChange={this.onChange}
-                                    value={this.state.password}
-                                    error={errors.password}
-                                    id="password"
-                                    type="password"
-                                    className={classnames("form-control", {
-                                        invalid: errors.password
-                                    })}
-                                />
-                                <span className="text-danger">{errors.password}</span>
-                                <p className="text-center pb-0 mt-2">
-                                    <button
-                                        type="submit"
-                                        className="btn btn-large btn-primary mt-2 px-5">
-                                        Login
-                                    </button>
-                                </p>                                
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    const { errors } = this.state;
+    return (
+      <div className="container">
+        <div className="row mt-5">
+          <div className="col-md-4 mx-auto mt-5 card shadow-lg">
+            <div className="card-body p-1">
+              <h2 className="text-center text-primary mt-3">Login</h2>
+              <a className="backToHome" href="/loginPanel" tag={Link}>
+                <h2>Home</h2>
+              </a>
+              <form noValidate onSubmit={this.onSubmit} className="white">
+                <label htmlFor="email">Email</label>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.email}
+                  error={errors.email}
+                  id="email"
+                  type="email"
+                  className={classnames("form-control", {
+                    invalid: errors.email,
+                  })}
+                />
+                <span className="text-danger">{errors.email}</span>
+                <br />
+                <label htmlFor="password">Password</label>
+                <input
+                  onChange={this.onChange}
+                  value={this.state.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                  className={classnames("form-control", {
+                    invalid: errors.password,
+                  })}
+                />
+                <span className="text-danger">{errors.password}</span>
+                <p className="text-center pb-0 mt-2">
+                  <button
+                    type="submit"
+                    className="btn btn-large btn-primary mt-2 px-5"
+                  >
+                    Login
+                  </button>
+                </p>
+              </form>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Login.propTypes = {
-    loginAdmin: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    errors: PropTypes.object.isRequired
+  loginAdmin: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
 };
-const mapStateToProps = state => ({
-    auth: state.auth,
-    errors: state.errors
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors,
 });
-export default connect(
-    mapStateToProps,
-    { loginAdmin }
-)(Login);
+export default connect(mapStateToProps, { loginAdmin })(Login);
